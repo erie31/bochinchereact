edf62473788696788f728049d40d61589969cfbd
Erié <erie31@gmail.com>
2022-08-17 21:00:49 -0300

    cartView

--- src/components/cart.js
+++ src/components/cart.js
@@ -5,12 +5,11 @@ import Table from 'react-bootstrap/Table';
 
 const CartD = () =>{
     const {cart, clearCart, removeItem} = useContext(CartContext)
+let totalCart = 0;
+let subTotal= 0;
+    
 
-    const total = cart.reduce((acc, sum) => {
-        return acc + sum.total
-    }, 0)
-
-    console.log(total)
+    
     return(
         <div className='table'>
             <h3>{cart.length === 0 ? 'No hay productos/servicios agregados' :  'Este es tu pedido'}</h3>
@@ -25,18 +24,21 @@ const CartD = () =>{
                 </thead>
                 <tbody>
                     {cart.map((c) => {
+
+                       totalCart += c.quantity*c.precio
+                       
                         return (
                         <tr key={c.id}>
                         <button className="bi bi-trash" alt='delete' onClick={() => removeItem(c.id) } id={c.id}/>
                         <td>{c.nombre}</td>
                         <td>${c.precio}</td>
                         <td>{c.quantity}</td>
-                        <td>${c.total}</td>
+                        <td>${subTotal}</td>
                         </tr>)
                     })}
                     <tr>
                     <td colSpan={4}>Total</td>
-                    <td>${total}</td>
+                    <td>${totalCart}</td>
                     </tr>
                 </tbody>
             </Table>

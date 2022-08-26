ff2f6b8d3db7a1cbbaf1615ef727faae9c6bc448
Erié <erie31@gmail.com>
2022-08-26 09:57:48 -0300

    formEnCheckout

--- src/components/Checkout/Checkout.jsx
+++ src/components/Checkout/Checkout.jsx
@@ -1,37 +1,53 @@
 import CartContext from "../../context/cartContext";
 import { db } from "../../services/firebase";
-import { useState, useContext } from "react";
-import {  addDoc,  collection,  getDocs,  query,  where,  documentId,  writeBatch,} from "firebase/firestore";
+import { useState, useContext, useEffect } from "react";
+import {  addDoc,  collection,  getDocs,  query,  where,  documentId,  writeBatch, serverTimestamp } from "firebase/firestore";
 import { useNavigate } from "react-router-dom";
 import swal from "sweetalert";
+import { Button } from "@mui/material";
+import TextField from '@mui/material/TextField';
+import validator from "validator";
 
-import Form from "../Form";
 
-const Checkout = () => {
+const Checkout = (props) => {
   const [isLoading, setIsLoading] = useState(false);
   const [orderCreated, setOrderCreated] = useState(false);
   const { cart, getQuantity, getTotal, clearCart } = useContext(CartContext);
-
   const navigate = useNavigate();
-
   const totalQuantity = getQuantity();
   const total = getTotal();
+
+  const [buyerName, setBuyerName] = useState("");
+  const [buyerEmail, setBuyerEmail] = useState("");
+  const [buyerEmailConf, setBuyerEmailConf] = useState("");
+  const [buyerPhone, setBuyerPhone] = useState("");
+  const [validName, setValidName] = useState(false);
+  const [validEmail, setValidEmail] = useState(false);
+  const [validEmailConf, setValidEmailConf] = useState(false);
+  const [validPhone, setValidPhone] = useState(false);
+
+  useEffect(() => {
+    setValidName(validator.isAlpha(buyerName, "es-ES", { ignore: " " }));
+    setValidEmail(validator.isEmail(buyerEmail));
+    setValidEmailConf(validator.equals(buyerEmail, buyerEmailConf));
+    setValidPhone(validator.isNumeric(buyerPhone, "es-ES"));
+}, [buyerName, buyerEmail, buyerEmailConf, buyerPhone]);
 
   const createOrder = async () => {
     setIsLoading(true);
     
     try {
-      const objOrder = {
-        nombre: "",
-        telefono:"",
-        email:"",
-
-
-        items: cart,
+        const objOrder = {
+            buyer: {
+                name: buyerName,
+                phone: buyerPhone,
+                email: buyerEmail
+            },
+            items: cart,
         totalQuantity,
         total,
-        date: new Date(),
-      };
+        date: serverTimestamp(),
+        }
 
       const ids = cart.map((prod) => prod.id);
 
@@ -77,7 +93,7 @@ const Checkout = () => {
         swal("No contamos con stock de algunos productos");
       }
     } catch (error) {
-      console.log(error);
+      console.log("Algo no anda bien",error);
     } finally {
       setIsLoading(false);
     }
@@ -90,16 +106,40 @@ const Checkout = () => {
   if (orderCreated) {
     return <h1>Orden realizada con éxito</h1>;
   }
+  const handleNameChange = (e) => {
+    setBuyerName(e.target.value);
+}
 
-  return (
+const handleEmailChange = (e) => {
+    setBuyerEmail(e.target.value);
+}
+
+const handleEmailConfChange = (e) => {
+    setBuyerEmailConf(e.target.value);
+}
+
+const handlePhoneChange = (e) => {
+    setBuyerPhone(e.target.value);
+}
+
+
+
+
+return (
     <>
-      <h1>Orden de compra</h1>
-      <h2><Form/></h2>
-      <button className="" onClick={createOrder}>
-        Realizar compra
-      </button>
+        <h2>Checkout Information:</h2>
+        <TextField className="formItems" error={buyerName !== "" && !validName} required variant="filled" label="Full Name" onChange={handleNameChange} value={buyerName} />
+        <TextField className="formItems" error={buyerPhone !== "" && !validPhone} required variant="filled" label="Phone Number" onChange={handlePhoneChange} value={buyerPhone} />
+        <TextField className="formItems" error={buyerEmail !== "" && !validEmail} required variant="filled" label="Email Address" onChange={handleEmailChange} value={buyerEmail} />
+        <TextField className="formItems" error={buyerEmailConf !== "" && !validEmailConf} required variant="filled" label="Confirm Email Address" onChange={handleEmailConfChange} value={buyerEmailConf} />
+       
+            <div className="cartButtons">
+                <Button onClick={createOrder} variant="contained" disabled={(!validName || !validEmail || !validEmailConf || !validPhone)} color="success">Complete Purchase</Button>
+                <Button onClick={clearCart} variant="contained" color="error">Clear Cart</Button>
+            </div>
+        
     </>
-  );
-};
+)
+}
 
 export default Checkout;
--- src/components/ItemDetailContainer/ItemDetailContainer.js
+++ src/components/ItemDetailContainer/ItemDetailContainer.js
@@ -1,25 +1,17 @@
 import { useState, useEffect } from "react";
-
 import ItemDetail from "../ItemDetail/ItemDetail";
-
 import { useParams } from "react-router-dom";
-
 import { getDoc, doc } from "firebase/firestore";
-
 import { db } from "../../services/firebase";
 
 const ItemDetailContainer = () => {
-
   const [product, setProduct] = useState();
 
   const { product: productId } = useParams();
 
   useEffect(() => {
-
     getDoc(doc(db, "products", productId))
-
       .then((res) => {
-
         const data = res.data();
 
         const productDb = { id: res.id, ...data };
@@ -27,29 +19,20 @@ const ItemDetailContainer = () => {
         setProduct(productDb);
 
         console.log(data);
-
       })
 
       .catch((error) => {
-
         console.log(error);
-
       });
-
   }, [productId]);
 
   return (
-
     <div>
-
       <h1>Detalle</h1>
 
       <ItemDetail {...product} />
-
     </div>
-
   );
-
 };
 
 export default ItemDetailContainer;
